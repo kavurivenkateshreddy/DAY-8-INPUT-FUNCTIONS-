498929dba92a720dfc1fe3a409934097e47081ed
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-05-18 20:32:41 +0530

    Add files via upload

---
+++ taking_input.py
@@ -0,0 +1,25 @@
+"""
+Date: 17th MAY 2022 Thursday
+Name: kavuri venkatesh reddy
+File Desc: input() Functions
+"""
+'''
+---> we use input() function to take input from thr user.
+---> whatever you enter as input,the input() function converts it into a strinf(str).
+---> if you enter an inteager(int) value still input() function convert it into a string(str).
+'''
+
+# a=5 ; b=5
+# print(a+b)
+
+# s1=input('Enter the Value for S1:')   #Enter the Value for S1:5
+# s2=input('Entet the Value for S2:')   #Enter the Value for S2:5
+# print('Concatenation',s1+s2)          #concatenation 55
+
+# s1=int(input('Enter the Value for S1:'))    #Enter the Value for S1:5
+# s2=int(input('Entet the Value for S2:'))    #Enter the Value for S2:5
+# print('Addition with int is',s1+s2)         #Addition with int is 10
+
+# s1=float(input('Enter the Value for S1:'))  #Enter the Value for S1:5
+# s2=float(input('Entet the Value for S2:'))  #Enter the Value for S2:5
+# print('Addition with float is',s1+s2)       #Addition with float is 10.0
